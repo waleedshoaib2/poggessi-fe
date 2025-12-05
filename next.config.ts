d123d64826e64e08f1c34f507814f7e396f39802
MohammadAliAlizada <muhdalializada@gmail.com>
2025-12-05 14:49:23 +0500

    config: Update Next.js image configuration to use `remotePatterns` for `localhost` and `storage.googleapis.com`.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["storage.googleapis.com"],
+     remotePatterns: ["localhost", "storage.googleapis.com"].map((elm) => ({ hostname: elm }))
   },
 };
 
